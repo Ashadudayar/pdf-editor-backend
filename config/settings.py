d1295001c9be7d6fa0c6c6da56e78c372436e73a
Ashad <saleem.udayar@gmail.com>
2025-10-30 12:37:09 +0300

    Add pdfdoor.com to CORS allowed origins

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://pdfdoor.com",
-    "https://www.pdfdoor.com"
+    "https://www.pdfdoor.com",
     "https://pdf-editor-frontend-flax.vercel.app",
 ]
 
